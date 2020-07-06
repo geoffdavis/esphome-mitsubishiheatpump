e0fdd6ba0724c4e629453a122515bb212d71f6d2
Geoff Davis <geoff@geoffdavis.com>
2020-07-06 09:19:24 -0700

    Bump version to 1.3.0

--- espmhp.h
+++ espmhp.h
@@ -25,7 +25,7 @@ using namespace esphome;
 
 static const char* TAG = "MitsubishiHeatPump"; // Logging tag
 
-static const char* ESPMHP_VERSION = "1.2.0";
+static const char* ESPMHP_VERSION = "1.3.0";
 
 /* If polling interval is greater than 9 seconds, the HeatPump
 library reconnects, but doesn't then follow up with our data request.*/
