1aa19b28883c38d692adbb6c08bf91001f33cdc7
Geoff Davis <geoff@geoffdavis.com>
2021-05-27 13:24:02 -0700

    Minor whitespace cleanup

--- components/mitsubishi_heatpump/climate.py
+++ components/mitsubishi_heatpump/climate.py
@@ -50,11 +50,11 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
         # Optionally override the supported ClimateTraits.
         cv.Optional(CONF_SUPPORTS, default={}): cv.Schema(
             {
-                cv.Optional(CONF_MODE, default=DEFAULT_CLIMATE_MODES): 
+                cv.Optional(CONF_MODE, default=DEFAULT_CLIMATE_MODES):
                     cv.ensure_list(climate.validate_climate_mode),
-                cv.Optional(CONF_FAN_MODE, default=DEFAULT_FAN_MODES): 
+                cv.Optional(CONF_FAN_MODE, default=DEFAULT_FAN_MODES):
                     cv.ensure_list(climate.validate_climate_fan_mode),
-                cv.Optional(CONF_SWING_MODE, default=DEFAULT_SWING_MODES): 
+                cv.Optional(CONF_SWING_MODE, default=DEFAULT_SWING_MODES):
                     cv.ensure_list(climate.validate_climate_swing_mode),
             }
         ),
