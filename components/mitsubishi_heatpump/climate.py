89fdf26b63c4d893af47eab3ac5d3987062aab07
ErikT <etalma@me.com>
2022-10-10 09:07:33 +0200

    Update climate.py
    Added additional (default) swing modes

--- components/mitsubishi_heatpump/climate.py
+++ components/mitsubishi_heatpump/climate.py
@@ -18,7 +18,7 @@ AUTO_LOAD = ["climate"]
 CONF_SUPPORTS = "supports"
 DEFAULT_CLIMATE_MODES = ["HEAT_COOL", "COOL", "HEAT", "DRY", "FAN_ONLY"]
 DEFAULT_FAN_MODES = ["AUTO", "DIFFUSE", "LOW", "MEDIUM", "MIDDLE", "HIGH"]
-DEFAULT_SWING_MODES = ["OFF", "VERTICAL"]
+DEFAULT_SWING_MODES = ["OFF", "VERTICAL", "HORIZONTAL", "BOTH"]
 
 MitsubishiHeatPump = cg.global_ns.class_(
     "MitsubishiHeatPump", climate.Climate, cg.PollingComponent
