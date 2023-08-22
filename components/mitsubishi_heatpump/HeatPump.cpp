4ff31249c1f0f0197649f90a944036874c92b951
Eric Chavet <echavet@gmail.com>
2023-08-22 17:22:11 +0200

    tst

--- components/mitsubishi_heatpump/HeatPump.cpp
+++ components/mitsubishi_heatpump/HeatPump.cpp
@@ -17,7 +17,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include "HeatPump.h"
-using namespace esphome;
+
 
 // Structures //////////////////////////////////////////////////////////////////
 
