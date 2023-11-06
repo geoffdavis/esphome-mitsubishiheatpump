8bcb920dc1c8608e2263ddb320b813bdb9b51b6b
markalston <markalston@users.noreply.github.com>
2023-11-06 08:01:49 -0700

    Update climate.py
    Removed pinned version from HeatPump library load.

--- components/mitsubishi_heatpump/climate.py
+++ components/mitsubishi_heatpump/climate.py
@@ -90,5 +90,5 @@ def to_code(config):
     cg.add_library(
         name="HeatPump",
         repository="https://github.com/markalston/HeatPump",
-        version="d6a29134401d7caae1b8fca9c452c8eb92af60c5",
+        #version="d6a29134401d7caae1b8fca9c452c8eb92af60c5",
     )
